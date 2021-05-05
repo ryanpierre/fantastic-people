2f88efbdf83ceb1faab1c41ae5d4d5180a293a0b
ryanpierre <info@ryanpierre.com>
2021-05-06 00:42:50 +0100

    add some config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>The Fantastic Peoples Awards 2021</h2>
+      <h3>
+        <code>VERSION 2.0</code>
+      </h3>
     </div>
   );
 }
