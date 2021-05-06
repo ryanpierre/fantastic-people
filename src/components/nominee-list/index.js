ff6c6a21cb2389728ef34a799c9d7f8ace982699
ryanpierre <info@ryanpierre.com>
2021-05-06 01:43:14 +0100

    create wired version

--- src/components/nominee-list/index.js
+++ src/components/nominee-list/index.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Nominee from '../nominee';
 import './index.css';
 
-let nomineesDB = [
-  { id: 0, name: 'Alex', votes: 0 },
-  { id: 1, name: 'Alice', votes: 0 },
-  { id: 2, name: 'Ben', votes: 0 },
-  { id: 3, name: 'Dana', votes: 0 },
-  { id: 4, name: 'Eddie', votes: 0 },
-  { id: 5, name: 'Eóin', votes: 0 },
-  { id: 6, name: 'Josué', votes: 0 },
-  { id: 7, name: 'Katerina', votes: 0 },
-  { id: 8, name: 'Kay', votes: 0 },
-  { id: 9, name: 'Leo', votes: 0 },
-  { id: 10, name: 'Ryan', votes: 0 },
-  { id: 11, name: 'Simo', votes: 0 },
-];
-
 const NomineeList = () => {
+  const [nominees, setNominees] = useState([]);
+
+  const fetchNominees = async () => {
+    const res = await fetch(
+      'https://shielded-meadow-19821.herokuapp.com/nominees',
+      { method: 'GET' }
+    );
+    const json = await res.json();
+
+    setNominees(json.nominees);
+  };
+
+  useEffect(() => fetchNominees(), []);
+
   return (
     <div className="nominee-list">
-      {nomineesDB.map((nominee) => (
-        <Nominee {...nominee} />
+      {nominees.map((nominee) => (
+        <Nominee key={`nominee-${nominee.id}`} {...nominee} />
       ))}
     </div>
   );
