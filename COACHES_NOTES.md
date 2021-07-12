6016f2e19bdcff18c400732e696fe6410ffc9433
ryanpierre <info@ryanpierre.com>
2021-07-12 17:07:51 +0100

    check in changes discussed during call until time to refine them

--- COACHES_NOTES.md
+++ COACHES_NOTES.md
@@ -1,11 +1,20 @@
 # Coaches Notes
+## Introduction
 
-## Flow 
+Open up a notepad and have a discussion about some things. We're mostly interested in helping them recall concepts from the SPA pill & video 
+
+- What is different about a single page application and the applications we've created thus far ?
+- What is a component ? What is an atomic component ?
+- Bottom line: React takes data and turns it into a view
+## Part 1: Anatomy of a component
+
+
+
 
 - Talk about components a bit
   - What is a component ?
-  - What do we think an atomic component is ?
-  - What do we think a composite component is ?
+  - What is state ? What types of things would I want in state ?
+  - What do we think an atomic/composite component is ?
 - Introduce the basic app in the state it's in currently
 - Show that we can actually run any javascript in our JSX if we want to !
 - Walk through the nominee list component and the magic JSX we can pull off with map
--- src/components/nominee-list/index.js
+++ src/components/nominee-list/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Nominee from '../nominee';
-import useNominees from '../../hooks/use-nominees';
+import nomineesAPI from '../../hooks/use-nominees';
 import './index.css';
 
+// Abstract add vote into the nominees api instead
+// demonstrate passing a callback, by passing the addVote method
+
+// END HERE SKIP USE EFFECT
+// Cheat in the nomineesAPI and make fetching nominees have a delay of 5s or something
+// use this to demonstrate a loading spinner with useEffect
+
+// Remove the use Effect in the hook, and make it a getNominees function instead we can call from a useEffect hook
+// in order to demonstrate the useEffect === componentDidMount data fetching use case
+
+// What if we wanted to find the winner ? How could we collect all the votes at any given time ?
+// ---> Hoisting state, passing callbacks as props
+
 const NomineeList = () => {
-  const [nominees] = useNominees();
+  const [nominees, addVote] = nomineesAPI();
+  const [loading, setLoading] = useState(false);
+  const [currentLeader, setCurrentLeader] = useState(null);
+
+  // const addVote = () => {
+  //   const response = fetch(`url/nominee/${id}`);
+
+  //   const { nominee } = response.json();
+
+  //   setVotes(nominee.votes);
+  // };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 5000);
+  }, []);
+
+  const calculateCurrentLeader = () => {};
 
   return (
     <div className="nominee-list">
+      <button>Get leader</button>
+      <p>The leader is: {currentLeader}</p>
       {nominees.map((nominee) => (
-        <Nominee key={`nominee-${nominee.id}`} {...nominee} />
+        <Nominee key={`nominee-${nominee.id}`} {...nominee} onVote={addVote} />
       ))}
     </div>
   );
